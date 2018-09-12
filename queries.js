b88a580ac305b6224560d29835d8ec28c9816b4a
Arvind Kalai Silvan <arvind_montfortsec@outlook.com>
2018-09-12 11:07:47 +0800

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,27 @@
+var pg = require('pg');
+
+var conString = "postgres://root:password@aa17gf7d0ugh9hp.crjstpn2rrf4.us-east-2.rds.amazonaws.com:5432/ebdb";
+
+//Initialise db
+function db_initialise(res,query){
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+	  if(err) {
+		return console.error('could not connect to postgres', err);
+	  }
+	  client.query(query, function(err, result) {
+		if(err) {
+		  return console.error('error running query', err);
+		}
+		res.send(res.rows);
+		client.end();
+	  });
+	});
+}
+function getAllUsers(res){
+	var query = 'SELECT * FROM COMPANY';
+	db_initialise(res,query);
+}
+module.exports = {
+	getAllUsers: getAllUsers
+};
